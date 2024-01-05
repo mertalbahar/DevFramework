70b1a47fbaf6d22f982b51872149d7a548a404de
MertalBahar <mertalbahar@windowslive.com>
2024-01-05 07:50:14 +0300

    Data Access procedures done. 1. IProductDal created and implemented from IEntityRepository for product. 2. Product created from using db product table and implemented from IEntity. 3. EfProductDal created and implemented from EfEntityRepositoryBase for product and NorthwindContext, IProductDal. 4. NorthwindContex created and impelented from DbContext. Database initializer set null for migrations because DbFirst. 5. ProductMap created and implemented from EntityTypeConfiguration.

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/Mappings/ProductMap.cs
@@ -0,0 +1,32 @@
+﻿using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework.Mappings
+{
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        // If you have DbFirst coding or you have db coded by another language,
+        // you have to connect your code with mapping
+        public ProductMap()
+        {
+            ToTable(@"Products", @"dbo");
+            HasKey(x =>  x.ProductId);
+
+            Property(x => x.ProductId).HasColumnName("ProductID");
+            Property(x => x.CategoryId).HasColumnName("CategoryID");
+            Property(x => x.SupplierId).HasColumnName("SupplierID");
+            Property(x => x.ProductName).HasColumnName("ProductName");
+            Property(x => x.QuantityPerUnit).HasColumnName("QuantityPerUnit");
+            Property(x => x.UnitPrice).HasColumnName("UnitPrice");
+            Property(x => x.UnitsInStock).HasColumnName("UnitsInStock");
+            Property(x => x.UnitsOnOrder).HasColumnName("UnitsOnOrder");
+            Property(x => x.ReorderLevel).HasColumnName("ReorderLevel");
+            Property(x => x.Discontinued).HasColumnName("Discontinued");
+        }
+    }
+}
