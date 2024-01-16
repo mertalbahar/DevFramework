a2a7287793f5711a6d331aca1180b81337c539c6
MertalBahar <mertalbahar@windowslive.com>
2024-01-16 11:27:53 +0300

    ProductManager created in business. FluentValidaton created. 1. IProductService created. 2. ProductManager created. 3. ProductValidator created, FluentValidaton package added. 4. ValidatorTool created. 5. FluentValidationAspect created, Postsharp package added. 6. ProductManagerTests created, Moq package added. Unit test cannot worked and passed, because Postsharp error occured.

---
+++ DevFramework.Northwind.Business/Concrete/Manager/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using DevFramework.Core.Aspects.Postsharp;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.Business.ValidationRules.FluentValidation;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Northwind.Business.Concrete.Manager
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        [FluentValidationAspect(typeof(ProductValidator))]
+        public Product Update(Product product)
+        {
+            return _productDal.Update(product);
+        }
+    }
+}
